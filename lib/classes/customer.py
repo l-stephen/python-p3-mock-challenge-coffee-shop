bddf260c8e568423d9f5598e1e75263f897f25e3
Stephen Lambert <97977357+l-stephen@users.noreply.github.com>
2023-05-10 10:21:40 -0600

    Update customer.py

--- lib/classes/customer.py
+++ lib/classes/customer.py
@@ -3,31 +3,29 @@ class Customer:
         self.name = name
         self._orders = []
         self._coffees = []
-        
+
     @property
     def name(self):
         return self._name
     
     @name.setter
-    def name(self, name):
-        if isinstance(name, str) and 1 <= len(name) <= 15:
-            self._name = name
+    def name(self, new_name):
+        if new_name and 1 <= len(new_name) <= 15:
+            self._name = new_name
         else:
             raise Exception
         
     def orders(self, new_order=None):
         from classes.order import Order
-        
-        if new_order and isinstance(new_order, Order):
+        if isinstance(new_order, Order):
             self._orders.append(new_order)
-            
+
         return self._orders
-    
+        
     def coffees(self, new_coffee=None):
         from classes.coffee import Coffee
-        
-        if new_coffee and isinstance(new_coffee, Coffee) and new_coffee not in self._coffees: 
-            
+        if isinstance(new_coffee, Coffee) and new_coffee not in self._coffees :
             self._coffees.append(new_coffee)
-            
-        return  self._coffees
+
+        return self._coffees
+   
